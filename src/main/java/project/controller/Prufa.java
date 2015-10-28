7009dd9e14efb131be139ac5e56a5e88b50c13da
beemmess <bms4@hi.is>
2015-10-28 14:12:59 +0000

    blabal
    blabla

---
+++ src/main/java/project/controller/Prufa.java
@@ -0,0 +1,10 @@
+package project.controller;
+
+import project.service.PostitNoteService;
+
+/**
+ * Created by bjarkimar23 on 28.10.2015.
+ */
+public class Prufa {
+    PostitNoteService postitNoteService;
+}
